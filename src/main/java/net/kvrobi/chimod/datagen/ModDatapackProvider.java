5c958caa47459d14ad9c44a7bda7573878b182f5
kvrobi <kvrobill@gmail.com>
2026-04-29 20:33:42 +0200

    model kesz, fp jon

--- src/main/java/net/kvrobi/chimod/datagen/ModDatapackProvider.java
+++ src/main/java/net/kvrobi/chimod/datagen/ModDatapackProvider.java
@@ -1,16 +1,25 @@
 package net.kvrobi.chimod.datagen;
 
 import net.kvrobi.chimod.ChiMod;
+import net.kvrobi.chimod.worldgen.ModBiomeModifiers;
+import net.kvrobi.chimod.worldgen.ModConfiguredFeatures;
+import net.kvrobi.chimod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
-    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder();
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootsrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(ChiMod.MOD_ID));
---
+++ src/main/java/net/kvrobi/chimod/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,24 @@
+package net.kvrobi.chimod.worldgen;
+
+import net.kvrobi.chimod.ChiMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.rmi.registry.Registry;
+
+public class ModBiomeModifiers {
+
+    public static void bootsrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(ChiMod.MOD_ID, name));
+    }
+}
